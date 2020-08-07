3f26abe02691be0f0021983703baf858a9e96009
Amit Raya <amit.raya@hotmail.com>
2020-08-07 20:14:03 +0530

    Add project files.

---
+++ SugarCalc/SugarCalc/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using SugarCalc.ViewModels;
+
+namespace SugarCalc.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
